b3433740b5ba59693377a00d57044a41c75a4dad
ChamashaFernando <chamashanethmi81@gmail.com>
2025-06-20 11:51:49 +0530

    Initial commit - add spring boot project

---
+++ src/main/java/lk/chamasha/online/book/store/OnlineBookStoreApiApplication.java
@@ -0,0 +1,13 @@
+package lk.chamasha.online.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/chamasha/online/book/store/OnlineBookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.online.book.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
